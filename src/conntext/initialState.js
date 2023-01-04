073b0df4b1686c50911959dac420b6713c2e0948
Ifeanyi Osi-Okeke <75077705+ifeanyiosi@users.noreply.github.com>
2023-01-05 00:20:24 +0100

    fix

--- src/conntext/initialState.js
+++ src/conntext/initialState.js
@@ -2,6 +2,11 @@ import { fetchUser } from "../utils/FetchLocalStorage";
 
 const userInfo = fetchUser();
 
+// export const initialState = {
+//   user: userInfo,
+// };
+
 export const initialState = {
   user: userInfo,
+  foodItems: null,
 };
