74a0192ff09efca339c8a587453bbad5bb86ce97
Ifeanyi Osi-Okeke <75077705+ifeanyiosi@users.noreply.github.com>
2023-01-08 05:29:14 +0100

    Update index.jsx

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -87,7 +87,7 @@ const Cart = () => {
             <div className="w-full flex items-center justify-between">
               <p className="text-gray-200 text-xl font-semibold"> Total</p>
               <p className="text-gray-200 text-xl font-semibold">
-                ₦{tot + 2.5}
+                ₦{tot + 1500}
               </p>
             </div>
             {user ? (
